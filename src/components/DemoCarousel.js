278f931b9a54d806812fc62989cc0a478d700a74
Denis Macagno <denis.macagno@gmail.com>
2020-05-27 11:48:19 -0300

    modificacion carousel mobile

--- src/components/DemoCarousel.js
+++ src/components/DemoCarousel.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
- 
+
 import banner1 from "../images/Banner1.png";
 import banner3 from "../images/Banner3.png";
 
@@ -10,22 +10,15 @@ class DemoCarousel extends Component {
         return (
             <Carousel infiniteLoop useKeyboardArrows autoPlay >
                 <div>
-                    <img src={banner1} />
+                    <img id="bannerImg1" src={banner1} />
                 </div>
                 <div>
-                    <img src={banner3} />
+                    <img id="bannerImg2" src={banner3} />
                 </div>
-                <div>
-                    <img src={banner1} />
-                </div>
-                <div>
-                    <img src={banner3} />
-                </div>
-                
+
             </Carousel>
         );
     }
 };
- 
+
 export default DemoCarousel;
- 
